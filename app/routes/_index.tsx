ecb704bbc8f8a8b3d295f742bc32630c2f79deaf
Collin Town <35181633+towner-10@users.noreply.github.com>
2024-01-27 13:50:53 -0500

    Scale to full page

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -29,13 +29,13 @@ export default function Index() {
 	const [acceptedLetters, setAcceptedLetters] = useState<string>('');
 
 	return (
-		<>
+		<main>
 			<header className="flex flex-row justify-end pt-5 pr-10 pb-5">
 				<ModeToggle />
 			</header>
-			<main className='grid h-screen grid-cols-4 p-10 pt-0 pr-10'>
+			<section className='grid grid-cols-4 p-10 pt-0 pr-10'>
 				<div className='col-span-1 flex flex-col ml-20'>
-					<img src={logo} className='w-48 h-22'></img>
+					<img src={logo} alt="Logo" className='w-48 h-22'></img>
 					<div className='flex flex-col pt-12 gap-0'>
 						<h2> {score} </h2>
 						<h3> MATCH SCORE </h3>
@@ -50,25 +50,20 @@ export default function Index() {
 							<h5> {acc}% </h5>
 						</div>
 					</div>
-
 					<div className='flex flex-col pt-12 gap-0'>
 						<h2> {hScore} </h2>
 						<h3> HIGH SCORE </h3>
 					</div>
-
-					
-
 					<div className='flex flex-col pt-10 gap-2'>
 						<Input onChange={(e) => setAcceptedLetters(e.target.value)}></Input>
 						<h3>FOCUS LETTERS</h3>
 					</div>
-
 					<Button className='text-white font-bold mt-12'>START MATCH</Button>
 				</div>
 				<div className='col-span-3 ml-20'>
 					<DrawingCanvas />
 				</div>
-			</main>
-		</>
+			</section>
+		</main>
 	);
 }
