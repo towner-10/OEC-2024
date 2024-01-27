c7e3e9c5d076c10b7d77bfb1a6d1cfaa117ab524
David Tam <dtam43@uwo.ca>
2024-01-27 11:37:18 -0500

    frontend styling

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -6,7 +6,7 @@ import DrawingCanvas from '~/components/drawing-canvas';
 
 import { ModeToggle } from '~/components/mode-toggle';
 import { useStore } from '~/lib/game-state';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 export const meta: MetaFunction = () => {
 	return [{ title: 'PictureThis' }, { name: 'description', content: 'Placeholder' }];
@@ -19,19 +19,52 @@ export default function Index() {
 		console.log(path);
 	}, [path]);
 
+	const [score, setScore] = useState<number>(0);
+	const [spd, setSpeed] = useState<number>(10);
+	const [acc, setAccuracy] = useState<number>(50);
+	const [hScore, setHighScore] = useState<number>(20000);
+	const [acceptedLetters, setAcceptedLetters] = useState<string>('');
+
 	return (
 		<>
 			<header className="flex flex-row justify-between items-center">
-				<>
-					<h1>Letter</h1>
-					<h1>Ninja</h1>
-				</>
+				<h1>
+					Letter Ninja
+				</h1>
 				<ModeToggle />
 			</header>
-			<main className="px-8">
-				<Input placeholder="enter accepted letters"></Input>
-				<DrawingCanvas />
-				<Button>Start the Game</Button>
+			<main className='grid grid-cols-3 p-10'>
+				<div className='col-span-1 flex flex-col gap-10'>
+					<div className='flex flex-col gap-2'>
+						<h2> {score} </h2>
+						<h3> MATCH SCORE </h3>
+					</div>
+					<div className='flex flex-col gap-2'>
+						<h2> {hScore} </h2>
+						<h3> HIGH SCORE </h3>
+					</div>
+
+					<div>
+						<div className='grid grid-cols-2 px-20 py-0 gapx-10 gapy-0'>
+							<h4> SPEED </h4>
+							<h5> {spd} letters/min</h5>
+						</div>
+						<div className='grid grid-cols-2 px-20 py-0 gapx-10 gapy-0'>
+							<h4> ACCURACY </h4>
+							<h5> {acc}% </h5>
+						</div>
+					</div>
+
+					<div className='flex flex-col gap-2'>
+						<Input onChange={(e) => setAcceptedLetters(e.target.value)}></Input>
+						<h3>FOCUS LETTERS</h3>
+					</div>
+
+					<Button>START MATCH</Button>
+				</div>
+				<div className='col-span-2 pl-10'>
+					<DrawingCanvas />
+				</div>			
 			</main>
 		</>
 	);
